72022c2e3f7baff268f583ef032f6af81d7a1e4b
Andreas Nickl <59257257+andncl@users.noreply.github.com>
2023-07-20 16:04:52 +1000

    Added opx connect function

--- arbok/core/program.py
+++ arbok/core/program.py
@@ -1,8 +1,16 @@
 from arbok.core.sequence import Sequence
+from qm.QuantumMachinesManager import QuantumMachinesManager
 
 class Program(Sequence):
     def __init__(self, name: str, sample, param_config=...):
         super().__init__(name, sample, param_config)
 
+        self.opx = None
+        self.job = None
+
     def run(self):
         return
+    
+    def connect_OPX(self, host_ip: str, name)
+        QMm = QuantumMachinesManager(host = host_ip, cluster_name = name)
+        Qm1 = QMm.open_qm(self.sample.config)
