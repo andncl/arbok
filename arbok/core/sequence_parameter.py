848f594ccdebca5e8842c18fd59cbf01624e9712
RF2v meas pc <siqd@outlook.com>
2023-09-12 14:54:27 +1000

    Reworked GettableParameter

--- arbok/core/sequence_parameter.py
+++ arbok/core/sequence_parameter.py
@@ -2,6 +2,8 @@ from qcodes.parameters import Parameter, ParameterWithSetpoints
 from qcodes.validators import Arrays
 from qm.qua import *
 
+import warnings
+
 class SequenceParameter(Parameter):
     """
     A parameter wrapper that adds the respective element as attribute
@@ -19,7 +21,6 @@ class SequenceParameter(Parameter):
         self.batched = False
         self.qua_var = None
 
-
     def __call__(self, *args, **kwargs):
         if len(args) == 1:
             self.set(*args)
@@ -28,70 +29,87 @@ class SequenceParameter(Parameter):
         else: 
             return self.get()
 
-class ReadParameter(ParameterWithSetpoints):
+    def set_on_program(self, *args):
+        self.root_instrument.settables.append(*args[0])
+
+class GettableParameter:
     """
-    A read parameter wrapper containing OPX result fetching logic
+    This is a valid Gettable not because of inheritance, but because it has the
+    expected attributes and methods.
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, name, readout, *args, **kwargs) -> None:
         """
-        Constructor for 'ReadParameter' class
+        Constructor class for ReadSequence class
+        Args:
+            name (dict): name of the GettableParameter
+            readout (Readout): Readout class summarizing data streams and variables
         """
-        super().__init__(*args, **kwargs)
-        self.result = None
-        self.counts_so_far = 0
-        self.last_result = None
-        self.buffer = None
-        self.result = None
+        self.name = name
+        self.unit = ""
+        self.label = ""
         self.batched = True
-        self.batch_size = self.root_instrument.sweep_size()
+        self.delay = 0.0
+
+        self.readout = readout
+        self.program = None
+        self.qm_job = None
+        self.result = None
+        self.buffer = None
+        self.buffer_val = None
+        self.shape = None
+        self.count_so_far = 0
+        self.last_result = None
+        self.batch_size = 0
         self.count = 0
 
-    def get_raw(self):
-        if not self.root_instrument.opx:
-            raise LookupError("Results cant be retreived without OPX")
-        
-        if self.result == None:
-            self.buffer = getattr(
-                self.root_instrument.qm_job.result_handles,self.name + '_buffer')
-            self.result = getattr(
-                self.root_instrument.qm_job.result_handles,self.name)
-            self.setpoints = tuple(self.root_instrument.settables) 
-            self.shape = tuple([len(x) for x in self.root_instrument.setpoints_grid])
-            self.batch_size = self.root_instrument.sweep_size()
-            #self.vals = Arrays(shape = self.shape) 
-            self.count += 1
+    def get(self):
+        """ Get method to retrieve a single batch of data from a running measurement"""
+        if self.result is None:
+            # Will be executed on the first call of get()
+            self.set_up_gettable_from_program()
+            if self.program.stream_mode == "pause_each": 
+                self.qm_job.resume()
             
-
-        else:
-            return self._fetch_from_opx()
+        self._fetch_from_opx()
+        if self.buffer_val is None:
+            warnings.warn("NO VALUE STREAMED!")
+        if self.program.stream_mode == "pause_each": 
+            self.qm_job.resume()
+        return self.buffer_val
+    
+    def set_up_gettable_from_program(self):
+        """ Set up Gettable attributes from running OPX """
+        self.program = self.readout.sequence.root_instrument
+        if not self.readout.sequence.root_instrument.opx:
+            raise LookupError("Results cant be retreived without OPX connected")
+        self.qm_job = self.program.qm_job
+        self.result = getattr( self.qm_job.result_handles, self.name )
+        self.buffer = getattr( self.qm_job.result_handles, self.name + '_buffer')
+        self.shape = tuple([len(x) for x in self.program.setpoints_grid])
+        self.batch_size = self.program.sweep_size()
 
     def _fetch_from_opx(self):
-        if not self.root_instrument.qm_job.is_paused() or self.result.count_so_far() < self.count*self.batch_size :
-            print(
-                'busy \n',
-                'paused;  ' + str(self.root_instrument.qm_job.is_paused()) + '\n',
-                'total counts: ' + str(self.result.count_so_far()) + '\n',
-                'registerd; ' + str(self.count*self.batch_size) + '\n',
-                end = '\r')
-            self._fetch_from_opx()
+        """ Fetches and returns data from OPX after results came in """
+        self.count_so_far = self.result.count_so_far()
+        if self.count_so_far > (self.count + 2)*self.batch_size:
+            warnings.warn("OVERHEAD of data on OPX! Try larger batches or other sweep type!")
 
-        else:
-            print(
-                'New result! \n',
-                'paused;  ' + str(self.root_instrument.qm_job.is_paused()) + '\n',
-                'total counts: ' + str(self.result.count_so_far()) + '\n',
-                'registerd; ' + str(self.count*self.batch_size) + '\n',
-                end = '\r')
-            self.root_instrument.qm_job.resume()
-            buffer_val = self.buffer.fetch_all()
-            if buffer_val is None:
-                self._fetch_from_opx()
-            else:
-                self.count +=1
-                print(buffer_val.reshape(list(self.shape)).astype(int))
-                return buffer_val.reshape(list(self.shape)).astype(int)
-        
+        self._wait_until_buffer_full()
+        self._fetch_opx_buffer()
+
+    def _wait_until_buffer_full(self):
+        while self.count_so_far < (self.count + 1)*self.batch_size:
+            # This loop is running until a batch with self.batch_size is ready
+            self.count_so_far = self.result.count_so_far()
+
+    def _fetch_opx_buffer(self):
+        self.buffer_val = self.buffer.fetch(-1)
+        if self.buffer_val is None:
+            raise ValueError("NO VALUE STREAMED")
+        self.count += 1
+
     def get_all(self):
+        """ Fetches ALL (not buffered) data """
         if self.result:
             return self.result.fetch_all()
         else:
