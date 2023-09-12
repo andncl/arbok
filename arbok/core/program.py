7c5cb8e8b4e0acdeec5a083127838d631cd4f4f6
RF2v meas pc <siqd@outlook.com>
2023-09-12 14:51:19 +1000

    Added get_program method to program

--- arbok/core/program.py
+++ arbok/core/program.py
@@ -1,5 +1,9 @@
 from arbok.core.sequence import Sequence
+
 from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm.qua import *
+
+import copy
 
 class Program(Sequence):
     def __init__(self, name: str, sample, param_config = {}):
@@ -10,8 +14,7 @@ class Program(Sequence):
         self.qm_job = None
         self.result_handles = None
 
-    def run(self):
-        return
+        self.stream_mode = "pause_each"
     
     def connect_OPX(self, host_ip: str):
         self.qmm = QuantumMachinesManager(host = host_ip)
@@ -21,9 +24,32 @@ class Program(Sequence):
     def run(self):
         self.qm_job = self.opx.execute(self.get_program())
         self.result_handles = self.qm_job.result_handles
-        self.qm_job.resume()
+        #self.qm_job.resume()
 
     def prepare_meas_ctrl(self, meas_ctrl):
         meas_ctrl.settables(self.settables)
         meas_ctrl.setpoints_grid(self.setpoints_grid)
         return self.settables
+
+    def get_program(self, simulate = False):
+        """
+        Runs the entire sequence by searching recursively through init, 
+        sequence and stream methods of all subsequences and their subsequences
+
+        Args:
+            simulate (bool): Flag whether program is simulated
+        """
+        with program() as prog:
+                self.recursive_qua_generation(seq_type = 'declare')
+                with infinite_loop_():
+                    #if True: #not simulate: # and self.stream_mode == "pause_each":
+                    pause()
+                    if True or simulate: # self.wait_for_trigger()
+                        self.recursive_sweep_generation(
+                            copy.copy(self.settables),
+                            copy.copy(self.setpoints_grid)
+                            )
+
+                with stream_processing():
+                    self.recursive_qua_generation(seq_type = 'stream')
+        return prog
